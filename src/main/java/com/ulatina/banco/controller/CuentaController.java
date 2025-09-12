b19809f17e54c59ecd0af68a023065ae23b397c1
Esteban Hernández Monge <esteban@MacBook-Pro-de-Esteban.local>
2025-09-12 17:08:36 -0600

    BNK-F-005 - SINPE MOVIL

--- src/main/java/com/ulatina/banco/controller/CuentaController.java
+++ src/main/java/com/ulatina/banco/controller/CuentaController.java
@@ -31,6 +31,12 @@ public class CuentaController implements Serializable {
     private int cuentaOrigenId = 3;
     private int cuentaDepositoId = 1;
 
+    //SINPE
+    private int cuentaSinpeOrigenId;
+    private String numeroDestinoSinpe;
+    private BigDecimal montoSinpe;
+    private String descripcionSinpe;
+
     public void cargarCuentas() {
         try {
             cuentas = cuentaService.obtenerCuentasPorClienteId(clienteId);
@@ -47,43 +53,40 @@ public class CuentaController implements Serializable {
         }
     }
 
-    
     public void realizarDeposito() {
 
         try {
-            
+
             Cuenta cuenta = cuentaService.buscarCuentaPorId(cuentaOrigenId);
             Cuenta cuenta2 = cuentaService.buscarCuentaPorId(cuentaDepositoId);
-            
+
             BigDecimal saldo = cuenta.getSaldo();
-            
+
             if (montoDeposito == null || montoDeposito.compareTo(BigDecimal.ZERO) <= 0) {
                 mostrarMensaje(FacesMessage.SEVERITY_WARN,
                         "Advertencia", "El monto debe ser mayor a cero");
                 return;
             }
-            
 
             if (cuenta.getEstado() != Cuenta.EstadoCuenta.ACTIVA) {
                 mostrarMensaje(FacesMessage.SEVERITY_WARN,
                         "Advertencia", "La cuenta debe estar activa para realizar depósitos");
                 return;
             }
-            
-            
-            if (cuenta.getMoneda() != cuenta2.getMoneda()){
+
+            if (cuenta.getMoneda() != cuenta2.getMoneda()) {
                 mostrarMensaje(FacesMessage.SEVERITY_WARN,
                         "Advertencia", "La cuenta destino contiene otro tipo de moneda");
                 return;
             }
-            
-            if (saldo == null || saldo.compareTo(BigDecimal.ZERO) <= 0){
+
+            if (saldo == null || saldo.compareTo(BigDecimal.ZERO) <= 0) {
                 mostrarMensaje(FacesMessage.SEVERITY_WARN,
                         "Advertencia", "Saldo insuficiente");
                 return;
             }
-            
-            if (montoDeposito.compareTo(saldo) > 0){
+
+            if (montoDeposito.compareTo(saldo) > 0) {
                 mostrarMensaje(FacesMessage.SEVERITY_WARN,
                         "Advertencia", "Sobrepasa sus fondos actuales");
                 return;
@@ -91,7 +94,6 @@ public class CuentaController implements Serializable {
 
             // Realizar el depósito
             boolean exito = cuentaService.realizarTransferencia(cuentaOrigenId, cuentaDepositoId, montoDeposito);
-            
 
             if (exito) {
                 mostrarMensaje(FacesMessage.SEVERITY_INFO,
@@ -100,7 +102,7 @@ public class CuentaController implements Serializable {
                 // Refrescar saldos y limpiar el monto
                 cargarCuentas();
                 montoDeposito = null;
-                
+
             } else {
                 mostrarMensaje(FacesMessage.SEVERITY_ERROR,
                         "Error", "No se pudo realizar el depósito");
@@ -113,19 +115,98 @@ public class CuentaController implements Serializable {
         }
     }
 
+    public void realizarSinpe() {
+        try {
+
+            // Validaciones básicas
+            if (cuentaSinpeOrigenId == 0) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN, "Advertencia", "Debe seleccionar una cuenta origen");
+                return;}
+
+            if (numeroDestinoSinpe == null || numeroDestinoSinpe.trim().isEmpty()) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN, "Advertencia", "Debe ingresar el número de teléfono destino");
+                return;}
+
+            if (montoSinpe == null || montoSinpe.compareTo(BigDecimal.ZERO) <= 0) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN, "Advertencia", "El monto debe ser mayor a cero");
+                return;}
+
+            if (descripcionSinpe == null || descripcionSinpe.trim().isEmpty()) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN, "Advertencia", "Debe ingresar una descripción");
+                return;}
+
+            // Validar cuenta origen
+            Cuenta cuentaSinpe = cuentaService.buscarCuentaPorId(cuentaSinpeOrigenId);
+            BigDecimal saldo = cuentaSinpe.getSaldo();
+
+            if (cuentaSinpe.getEstado() != Cuenta.EstadoCuenta.ACTIVA) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN, "Advertencia", "La cuenta debe estar activa para realizar transferencias");
+                return;}
+
+            // Validar saldo suficiente
+            if (saldo == null || saldo.compareTo(BigDecimal.ZERO) <= 0) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN,
+                        "Advertencia", "Saldo insuficiente");
+                return;}
+            
+            if (montoSinpe.compareTo(saldo) > 0) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN,
+                        "Advertencia", "Fondos insufucientes");
+                return;
+            }
+
+            // Para no permitir transferencia a cuentas dolares
+            if (cuentaSinpe.getMoneda() != Cuenta.Moneda.CRC) {
+                mostrarMensaje(FacesMessage.SEVERITY_WARN,
+                        "Advertencia", "SINPE móvil solo permite transferencias en colones");
+                return;
+            }
+
+            // Realizar la transferencia SINPE
+            boolean transferencia = cuentaService.realizarTransferenciaSinpe(cuentaSinpeOrigenId, montoSinpe);
+
+            if (transferencia) {
+                mostrarMensaje(FacesMessage.SEVERITY_INFO, "SINPE exitoso", "Transferencia realizada exitosamente."
+                        + " Numero destino: "+ numeroDestinoSinpe
+                        + " Monto: " + montoSinpe
+                        + " Descripción: " + descripcionSinpe);
+
+                // Refrescar saldos y limpiar formulario
+                cargarCuentas();
+                limpiarFormularioSinpe();
+
+            } else {
+                mostrarMensaje(FacesMessage.SEVERITY_ERROR,
+                        "Error", "No se pudo realizar la transferencia SINPE");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            mostrarMensaje(FacesMessage.SEVERITY_ERROR, "Error del sistema", "Error al realizar SINPE: " + e.getMessage());
+        }
+    }
+
+    private void limpiarFormularioSinpe() {
+        cuentaSinpeOrigenId = 0;
+        numeroDestinoSinpe = null;
+        montoSinpe = null;
+        descripcionSinpe = null;
+    }
+
     // Método de ayuda para mostrar los mensajes correspondientes
     private void mostrarMensaje(FacesMessage.Severity severity, String error, String detallado) {
         FacesContext.getCurrentInstance()
                 .addMessage(null, new FacesMessage(severity, error, detallado));
     }
 
-    
-    
-    public BigDecimal getMontoDeposito() {return montoDeposito;}
+    public BigDecimal getMontoDeposito() {
+        return montoDeposito;
+    }
 
-    public void setMontoDeposito(BigDecimal montoDeposito) {this.montoDeposito = montoDeposito;}
+    public void setMontoDeposito(BigDecimal montoDeposito) {
+        this.montoDeposito = montoDeposito;
+    }
 
-    
     public List<Cuenta> getCuentas() {
         if (cuentas == null) {
             cargarCuentas();
@@ -137,8 +218,44 @@ public class CuentaController implements Serializable {
         this.cuentas = cuentas;
     }
 
-    public int getClienteId() {return clienteId;}
+    public int getClienteId() {
+        return clienteId;
+    }
 
-    public void setClienteId(int clienteId) {this.clienteId = clienteId;}
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public int getCuentaSinpeOrigenId() {
+        return cuentaSinpeOrigenId;
+    }
+
+    public void setCuentaSinpeOrigenId(int cuentaSinpeOrigenId) {
+        this.cuentaSinpeOrigenId = cuentaSinpeOrigenId;
+    }
+
+    public String getNumeroDestinoSinpe() {
+        return numeroDestinoSinpe;
+    }
+
+    public void setNumeroDestinoSinpe(String numeroDestinoSinpe) {
+        this.numeroDestinoSinpe = numeroDestinoSinpe;
+    }
+
+    public BigDecimal getMontoSinpe() {
+        return montoSinpe;
+    }
+
+    public void setMontoSinpe(BigDecimal montoSinpe) {
+        this.montoSinpe = montoSinpe;
+    }
+
+    public String getDescripcionSinpe() {
+        return descripcionSinpe;
+    }
+
+    public void setDescripcionSinpe(String descripcionSinpe) {
+        this.descripcionSinpe = descripcionSinpe;
+    }
 
 }
